6669f9a465bdeeea1c3b97d7d5b105e3e7b845b5
David Renshaw <david@sandstorm.io>
2017-04-03 14:36:45 -0400

    use 'extract' branch of z3

--- src/constraints.rs
+++ src/constraints.rs
@@ -1,4 +1,5 @@
 use rustc::mir;
+use z3;
 
 use value::PrimValKind;
 
@@ -10,8 +11,8 @@ pub struct ConstraintContext {
 
 #[derive(Clone, Debug)]
 pub enum Val {
-    Variable(usize),
-    Constant(u128),
+    _Variable(usize),
+    _Constant(u128),
 }
 
 #[derive(Clone, Debug)]
@@ -26,5 +27,38 @@ impl ConstraintContext {
             variables: Vec::new(),
             constraints: Vec::new(),
         }
+    }
+
+    pub fn _is_feasible(&self) -> bool {
+        let cfg = z3::Config::new();
+        let ctx = z3::Context::new(&cfg);
+        let x = ctx.named_bitvector_const("x", 8 * 1024);
+        let y = x.extract(15, 8);
+        let z = x.extract(7, 0);
+
+        let y1 = x.extract(151, 144);
+        let z1 = x.extract(150, 143);
+
+        let solver = z3::Solver::new(&ctx);
+        solver.assert(&y._eq(&z).not());
+        solver.assert(&y1._eq(&z1).not());
+
+        assert!(solver.check());
+
+        let model = solver.get_model();
+        //    let xv = model.eval(&x).unwrap().as_i64().expect("1");
+        let yv = model.eval(&y).unwrap().as_i64().expect("1");
+        let zv = model.eval(&z).unwrap().as_i64().expect("2");
+        //    println!("x: {:x}", xv);
+        println!("y: {:x}", yv);
+        println!("z: {:x}", zv);
+
+
+        let yv1 = model.eval(&y1).unwrap().as_i64().expect("11");
+        let zv1 = model.eval(&z1).unwrap().as_i64().expect("21");
+        println!("y1: {:x}", yv1);
+        println!("z1: {:x}", zv1);
+
+        false
     }
 }
