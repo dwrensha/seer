dca6362b3adf5c68a3db62524157a279b86d23f6
David Renshaw <david@sandstorm.io>
2017-06-06 13:36:17 -0400

    make sure the indexing test has only one possible panic

--- tests/symbolic/indexing.rs
+++ tests/symbolic/indexing.rs
@@ -4,8 +4,11 @@ fn main() {
     let mut stdin = ::std::io::stdin();
     stdin.read(&mut data[..]).unwrap();
 
+    // Make sure there is only one possible way to hit the panic.
+    if data[0] >= 127 { return }
+
     let mut v: Vec<u16> = Vec::new();
-    for idx in 0..128 {
+    for idx in 0..128 { // going up to 256 takes too long
         v.push(idx);
     }
 
