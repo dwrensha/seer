80e0fad033b690bc832ebd31373f065971661d4e
David Renshaw <david@sandstorm.io>
2017-06-07 13:28:52 -0400

    pull the in the base64 example

---
+++ example/base64/README.md
@@ -0,0 +1,14 @@
+Build with:
+
+```
+RUSTFLAGS="-Z always-encode-mir" cargo build
+```
+
+
+Then run, in the seer top-level directory:
+
+```
+cargo run --bin run_symbolic --  -L dependency=./example/base64/target/debug/deps --extern base64=./example/base64/target/debug/deps/libbase64-671f71aca8a0bd16.rlib  ./example/base64/src/main.rs
+```
+
+(You'll probably need to adjust the hash on the rlib.)
---
+++ example/base64/src/main.rs
@@ -0,0 +1,28 @@
+extern crate base64;
+
+fn main() {
+    use std::io::Read;
+    let mut data: Vec<u8> = vec![0; 12];
+    let mut stdin = ::std::io::stdin();
+    stdin.read(&mut data[..]).unwrap();
+
+    let data_copy = data.clone();
+
+    let encoded = ::base64::encode(&data[..]);
+    if encoded.starts_with("dGhpcyBpcy") {
+        panic!()
+    }
+
+/*
+    match ::base64::decode(encoded.as_bytes()) {
+        Ok(v) => {
+            if &v[..] != &data_copy[..] {
+                panic!()
+            }
+        }
+        Err(e) => {
+            panic!()
+        }
+    }
+*/
+}
