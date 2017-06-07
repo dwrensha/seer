58e5b1c1c5dfbd3d5d6eda9e225a5d19ab41fee4
David Renshaw <david@sandstorm.io>
2017-06-07 15:59:19 -0400

    Add fuzzy-pickles example. Doesn't work due to lack of libstd mir.

---
+++ example/fuzzy-pickles/src/main.rs
@@ -0,0 +1,27 @@
+extern crate fuzzy_pickles;
+
+pub fn main() {
+    use std::io::Read;
+    let mut data: Vec<u8> = vec![0; 15];
+    let mut stdin = ::std::io::stdin();
+    stdin.read(&mut data[..]).unwrap();
+
+    for idx in 0..data.len() {
+        if data[idx] > 127 {
+            return; // avoid invalid utf8 by exiting early
+        }
+    }
+
+    let mut buf = String::new();
+    {
+        let mut buf_bytes = unsafe { buf.as_mut_vec() };
+        for idx in 0..data.len() {
+            buf_bytes.push(data[idx]);
+        }
+    }
+
+
+    let _ = ::fuzzy_pickles::parse_rust_file(&buf);
+
+    
+}
