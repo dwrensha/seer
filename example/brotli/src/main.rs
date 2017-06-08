bad55c5510ec0dedb25fe65cb90e0120e79b5614
David Renshaw <david@sandstorm.io>
2017-06-07 22:28:00 -0400

    add brotli example

---
+++ example/brotli/src/main.rs
@@ -0,0 +1,16 @@
+extern crate brotli;
+
+pub fn main() {
+    use std::io::{Cursor, Read};
+    let mut data: Vec<u8> = vec![0; 32];
+    let mut stdin = ::std::io::stdin();
+    stdin.read(&mut data[..]).unwrap();
+    let data = &data[..];
+
+    let mut data_reader = Cursor::new(data);
+    let mut result = Vec::with_capacity(data.len());
+
+    let mut de = brotli::Decompressor::new(&mut data_reader, data.len());
+
+    let _ = de.read_exact(&mut result);
+}
