8d5a7b0457bcb57f4da2e37b03568db582775f5e
David Renshaw <david@sandstorm.io>
2017-06-22 16:43:44 +0900

    avoid heap allocations in standalone/sort example

--- example/standalone/sort.rs
+++ example/standalone/sort.rs
@@ -1,12 +1,12 @@
 fn main() {
     use std::io::{stdin, Read};
-    let mut data: Vec<u8> = vec![0; 5];
+    let mut data = [0; 5];
     stdin().read_exact(&mut data[..]).unwrap();
 
     data.sort();
 
     let mut floor = 0;
-    for d in data {
+    for &d in data.iter() {
         if d < floor {
             panic!()
         }
