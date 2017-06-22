b4daf7607dd30c6e24731090b2bf3d07cbd517ef
David Renshaw <david@sandstorm.io>
2017-06-22 16:42:33 +0900

    avoid heap allocations in standalone/base64 example

--- example/standalone/base64.rs
+++ example/standalone/base64.rs
@@ -1,12 +1,19 @@
 // SEER lets us decode base64, given only an encoder function!
 
+const BUFFER_LENGTH: usize = 1024;
+
 fn main() {
     use std::io::Read;
-    let value_to_decode = "aGVsbG8gd29ybGQh";
-    let mut data: Vec<u8> = vec![0; (value_to_decode.len() + 3) / 4 * 3];
-    std::io::stdin().read_exact(&mut data[..]).unwrap();
+    let value_to_decode = b"aGVsbG8gd29ybGQh";
 
-    let result = base64_encode(&data[..]);
+    // We avoid heap allocations because they require liballoc to be compiled with MIR.
+    // See https://github.com/dwrensha/seer/issues/1
+    let mut data = [0u8; BUFFER_LENGTH];
+    let input_len = (value_to_decode.len() + 3) / 4 * 3;
+    std::io::stdin().read_exact(&mut data[..input_len]).unwrap();
+
+    let mut result = [0; BUFFER_LENGTH];
+    base64_encode(&data[..input_len], &mut result[..]);
     if result.starts_with(value_to_decode) {
         // This will panic on the input that encodes to `value_to_decode`.
         panic!("we found it!");
@@ -14,13 +21,10 @@ fn main() {
 }
 
 // copied from rustc_serialize
-fn base64_encode(input: &[u8]) -> String {
+fn base64_encode(input: &[u8], output: &mut [u8]) {
     static BYTES: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    let mut result = String::new();
     {
         let len = input.len();
-        let output = unsafe { result.as_mut_vec() };
-        *output = vec![b'='; (len + 2) / 3 * 4];
         let mod_len = len % 3;
         {
             let mut s_in = input[..len - mod_len].iter().map(|&x| x as u32);
@@ -56,5 +60,4 @@ fn base64_encode(input: &[u8]) -> String {
             }
         }
     }
-    result
 }
