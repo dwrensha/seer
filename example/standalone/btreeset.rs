8a9cc053857f4d49b5fce73bd8752f64933744e1
David Renshaw <david@sandstorm.io>
2017-06-22 16:49:24 +0900

    avoid heap allocations in standalone/btreeset example

--- example/standalone/btreeset.rs
+++ example/standalone/btreeset.rs
@@ -2,20 +2,20 @@ use std::collections::BTreeSet;
 
 fn main() {
     use std::io::Read;
-    let mut data: Vec<u8> = vec![0; 17];
+    let mut data = [0; 17];
     let mut stdin = ::std::io::stdin();
     stdin.read_exact(&mut data[..]).unwrap();
 
     let mut heap = BTreeSet::new();
-    for d in data {
+    for &d in data.iter() {
         heap.insert(d);
     }
 
     let mut floor = 0;
-    for d in heap.iter() {
-        if *d < floor {
+    for &d in heap.iter() {
+        if d < floor {
             panic!()
         }
-        floor = *d;
+        floor = d;
     }
 }
