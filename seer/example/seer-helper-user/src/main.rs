63afc218ace2bd480416670b4d063ffb3e2b2865
nilfit <nils.fitinghoff@gmail.com>
2018-06-25 15:52:20 +0200

    small fixes

--- seer/example/seer-helper-user/src/main.rs
+++ seer/example/seer-helper-user/src/main.rs
@@ -3,13 +3,15 @@ extern crate seer_helper;
 seer_helper_init!();
 
 #[derive(Debug)]
-struct Test {
+struct MyStruct {
     a: u32,
     b: u64,
 }
 
 fn main() {
-    let mut t = Test {a:0,b:0};
+    let mut t = MyStruct { a: 0, b: 0 };
     seer_helper::mksym(&mut t);
-    if t.a == 123 && t.b == 321 {panic!()}
+    if t.a == 123 && t.b == 321 {
+        panic!();
+    }
 }
